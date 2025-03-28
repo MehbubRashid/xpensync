d79954068223e191f94be5649d67dded8488a02d
MehbubRashid <mehbub.rabu@gmail.com>
2025-03-28 15:24:59 +0600

    integrated posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    ui_host: 'https://us.posthog.com'
+}
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+    <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={options}
+    >
+        <App />
+    </PostHogProvider>
+);
